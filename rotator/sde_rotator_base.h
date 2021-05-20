a95cb4b669db2b638f0439c1dab573df2b08773c
Andhavarapu Karthik <kartkart@codeaurora.org>
2021-05-20 12:59:43 +0530

    disp: rotator: add rev checks for rotator for blair target
    Add revision checks required for rotator for blair
    target.
    
    Change-Id: I04b107b5c85a20585a473a0ace56be07b26a357f
    Signed-off-by: Andhavarapu Karthik <kartkart@codeaurora.org>
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- rotator/sde_rotator_base.h
+++ rotator/sde_rotator_base.h
@@ -48,6 +48,7 @@
 #define SDE_MDP_HW_REV_600	SDE_MDP_REV(6, 0, 0)    /* msmnile+ v1.0 */
 #define SDE_MDP_HW_REV_630	SDE_MDP_REV(6, 3, 0)	/* bengal v1.0 */
 #define SDE_MDP_HW_REV_660	SDE_MDP_REV(6, 6, 0)	/* holi */
+#define SDE_MDP_HW_REV_690	SDE_MDP_REV(6, 9, 0)	/* blair */
 #define SDE_MDP_HW_REV_860	SDE_MDP_REV(8, 6, 0)	/* Ravelin */
 
 #define SDE_MDP_VBIF_4_LEVEL_REMAPPER	4
