453b2d3ec39a6f700e953c08249cad5126003a6e
Pavel Dubrova <pashadubrova@gmail.com>
2025-04-08 07:04:00 +0300

    msm_gem: Check coherency in dumb buffer creation
    Some targets do not support DMA coherency.
    If DMA coherency is not supported then use WC PAT.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- msm/msm_gem.c
+++ msm/msm_gem.c
@@ -700,10 +700,17 @@ void msm_gem_aspace_domain_attach_detach_update(
 int msm_gem_dumb_create(struct drm_file *file, struct drm_device *dev,
 		struct drm_mode_create_dumb *args)
 {
+	uint32_t flags = MSM_BO_SCANOUT;
+
+	if (dev && dev->dev && dev_is_dma_coherent(dev->dev))
+		flags |= MSM_BO_CACHED;
+	else
+		flags |= MSM_BO_WC;
+
 	args->pitch = align_pitch(args->width, args->bpp);
 	args->size  = PAGE_ALIGN(args->pitch * args->height);
 	return msm_gem_new_handle(dev, file, args->size,
-			MSM_BO_SCANOUT | MSM_BO_CACHED, &args->handle, "dumb");
+			flags, &args->handle, "dumb");
 }
 
 int msm_gem_dumb_map_offset(struct drm_file *file, struct drm_device *dev,
